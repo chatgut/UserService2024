f060bc5f3211e64e5e76fdd7fd611acc1ca29205
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-05-06 12:45:51 +0200

    init project

---
+++ src/main/java/org/iths/userservice2024/UserService2024Application.java
@@ -0,0 +1,13 @@
+package org.iths.userservice2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserService2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserService2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/iths/userservice2024/UserService2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iths.userservice2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserService2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
