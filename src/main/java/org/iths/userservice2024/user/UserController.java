0f9ed09c175913b7d825ef5b5b45a425c9a86aea
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-05-06 14:15:40 +0200

    Add CRUD methods

--- src/main/java/org/iths/userservice2024/user/UserController.java
+++ src/main/java/org/iths/userservice2024/user/UserController.java
@@ -1,7 +1,35 @@
 package org.iths.userservice2024.user;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(String userId) {
+        return userService.findByUserId(userId);
+    }
+    @PostMapping
+    void createUser(@RequestBody User user) {
+        userService.createUser(user);
+    }
+    @GetMapping("/loggedin/{userId}")
+    Boolean isLoggedIn(@PathVariable String userId) {
+        return userService.isLoggedIn(userId);
+    }
+    @PutMapping("/update/{userId}")
+    void updateUser(@PathVariable String userId, @RequestBody User user) {
+        userService.updateUser(userId, user);
+    }
+    @DeleteMapping("/delete/{userId}")
+    void deleteUser(@PathVariable String userId) {
+        userService.deleteUser(userId);
+    }
+
 }
