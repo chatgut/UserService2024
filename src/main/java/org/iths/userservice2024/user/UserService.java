7bb1a728341d5b3cc6efa8b3059f689ab0ec509c
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-05-06 14:16:00 +0200

    Update UserService.java

--- src/main/java/org/iths/userservice2024/user/UserService.java
+++ src/main/java/org/iths/userservice2024/user/UserService.java
@@ -4,4 +4,35 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findByUserId(String userId) {
+        return userRepository.findByUserId(userId);
+    }
+
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
+
+    public Boolean isLoggedIn(String userId) {
+        User user = userRepository.findByUserId(userId);
+        if (user == null) return false;
+        return user.getLoggedIn();
+    }
+
+    public void updateUser(String userId, User user) {
+       var existingUser = userRepository.findByUserId(userId);
+       existingUser.setLoggedIn(user.getLoggedIn());
+       existingUser.setPassword(user.getPassword());
+       existingUser.setUsername(user.getUsername());
+       userRepository.save(existingUser);
+    }
+
+    public void deleteUser(String userId) {
+        userRepository.deleteByUserId(userId);
+    }
 }
